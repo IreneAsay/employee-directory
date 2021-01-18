aa4e30dafd4d791471b82ce893a309babf600bea
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-18 17:59:29 +0800

    deleted uncessary files and added a FilterName.js

---
+++ src/FilterName.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FilterName extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default FilterName;
