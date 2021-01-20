1c8e49cb736459b99b77e618b420d07f0702b70c
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-20 22:36:40 +0800

    updated arrow functions

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,19 @@
 import axios from "axios";
 
 // get users function
-export default {
-    getUsers: function () {
-        return axios.get("https://randomuser.me/api/?results=200&nat=us");
-    }
+export const getUsers = () => {
+    axios
+        .get("https://randomuser.me/api/?results=200&nat=us")
+        .then((response) => {
+            console.log(response.data);
+            // handle success
+            return response;
+        })
+        .catch((err) => {
+            // handle error
+            console.log(err);
+        })
+        .then(() => {
+            // always executed
+        });
 };
