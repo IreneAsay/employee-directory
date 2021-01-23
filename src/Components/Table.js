31bad54ea8edc74bb5e0a498cb5fb56eaa66d683
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-23 17:49:52 +0800

    added a key to give the elements identity

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -7,18 +7,22 @@ const Table = (props) => {
                 <thead className="thead-light">
                     <tr>
                         <th scope="col">#</th>
+                        <th scope="col">Location</th>
                         <th scope="col">First</th>
                         <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email Address</th>
                     </tr>
                 </thead>
                 <tbody>
                     {props.users.map((user, i) => (
-                        <tr>
+                        <tr key={i}>
                             <th scope="row">{i + 1}</th>
+                            <td>{user.location.state}</td>
                             <td>{user.name.first}</td>
                             <td>{user.name.last}</td>
                             <td>{user.phone}</td>
+                            <td>{user.email}</td>
                         </tr>
                     ))}
 
