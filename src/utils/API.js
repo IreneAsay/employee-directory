f217434de7d4b0280cf44c61d7c89d4776d78c6c
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-23 17:50:59 +0800

    removed old API statement

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,18 +2,5 @@ import axios from "axios";
 
 // get users function
 export const getUsers = () => {
-    axios
-        .get("https://randomuser.me/api/")
-        .then((response) => {
-            console.log(response.data);
-            // handle success
-            return response;
-        })
-        .catch((err) => {
-            // handle error
-            console.log(err);
-        })
-        .then(() => {
-            // always executed
-        });
+    return axios.get("https://randomuser.me/api/?results=50&nat=us")
 };
