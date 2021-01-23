518213de449e96f4b2d0f9188ea492060704b609
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-24 03:35:15 +0800

    replaced form with div so the form will not be emptied out when the enter is clicked

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,13 +4,13 @@ const SearchBar = ({ handleSearchChange }) => {
     return (
         <div className="row col-12 bg-dark">
             <h1 className="col-lg-5 col-md col-sm-12 text-info ">Employee Directory</h1>
-            <form className="col-lg-3 col-md-4 col-sm-12 m-3 text-center">
+            <div className="col-lg-3 col-md-4 col-sm-12 m-3 text-center">
                 <input className="col-12 shadow" placeholder="Enter first and last name here..."
                     onChange={
                         e => handleSearchChange(e)
                     }
                 />
-            </form>
+            </div>
         </div>
     );
 };
