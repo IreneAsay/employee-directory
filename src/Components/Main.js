a172fdcbc9e5d54489fae8a23609c5bbfb0da381
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-23 20:57:09 +0800

    updated searched name format

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
-import NavBar from './NavBar'
-import { getUsers } from '../utils/API'
-import Table from './Table'
-import { isDOMComponentElement } from 'react-dom/test-utils';
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import { getUsers } from "../utils/API";
+import Table from "./Table";
+import { isDOMComponentElement } from "react-dom/test-utils";
 
 class Main extends Component {
-
     state = {
-        users: []
-    }
+        users: [],
+    };
     componentDidMount() {
         getUsers().then((res) => {
-            console.log(res.data.results)
-            this.setState({ users: res.data.results })
-        })
-    }
-
-    handleSearchChange = event => {
-        const filter = event.target.value;
-        const filteredList = this.state.users.filter(item => {
-            // Logic to filter users by state
-            console.log(item)
-            return item.name.first === filter;
+            console.log(res.data.results);
+            this.setState({ users: res.data.results });
         });
-        this.setState({ users: filteredList });
     }
 
-
+    handleSearchChange = (event) => {
+        const filter = event.target.value;
+        const filteredList = this.state.users.filter((item) => {
+            let values =
+                item.name.first.toLowerCase() + " " + item.name.last.toLowerCase();
+            console.log(filter, values);
+            if (values.indexOf(filter.toLowerCase()) !== -1) {
+                return item;
+            }
+        });
+        this.setState({ filteredUsers: filteredList });
+    };
 
     render() {
         return (
