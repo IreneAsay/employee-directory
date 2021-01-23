ddccfbd1959e640c050922f52133eb3d89f71e8c
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-23 15:49:32 +0800

    added state and lifecycle method

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar'
 import { getUsers } from '../utils/API'
+import Table from './Table'
 
 class Main extends Component {
-    onChange = e => {
-        getUsers(function (res) {
-            console.log(res)
+
+    state = {
+        users: []
+    }
+    componentDidMount() {
+        getUsers().then((res) => {
+            console.log(res.data.results)
+            this.setState({ users: res.data.results })
         })
     }
+
+    handleSearchChange = event => {
+        console.log(event.target.value);
+        const filter = event.target.value;
+        const filteredList = this.state.users.filter(item => {
+            // Logic to filter users
+        });
+        this.setState({ filteredUsers: filteredList });
+    }
+
+
+
     render() {
         return (
             <>
-                <NavBar onChange={this.onChange} />
+                <NavBar handleSearchChange={this.handleSearchChange} />
+                <Table users={this.state.users} />
             </>
         );
     }
