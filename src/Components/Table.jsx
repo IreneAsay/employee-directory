9b7a54332ec2fb19d47c7ecddcdcfbdee7f1cc38
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-26 10:48:28 +0800

    added onClick to the Location table head

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,39 +1,48 @@
-import React from 'react';
+import React from "react";
 
 const Table = (props) => {
-    return (
-        <div>
-            <table className="table table-striped ">
-                <thead className="bg-primary text-white">
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Photo</th>
-                        <th scope="col">Location</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Phone</th>
-                        <th scope="col">Email Address</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.users.map((user, i) => (
-                        <tr className="table-primary" key={i}>
-                            <th scope="row">{i + 1}</th>
-                            <td>
-                                <img src={user.picture.thumbnail} alt={user.name.first + ' ' + user.name.last} />
-                            </td>
-                            <td>{user.location.state}</td>
-                            <td>{user.name.first}</td>
-                            <td>{user.name.last}</td>
-                            <td>{user.phone}</td>
-                            <td>{user.email}</td>
-                        </tr>
-                    ))}
-
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div>
+      <table className="table table-striped ">
+        <thead className="bg-primary text-white">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Photo</th>
+            <th
+              scope="col"
+              onClick={() => {
+                props.handleSort();
+              }}
+            >
+              Location
+            </th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.users.map((user, i) => (
+            <tr className="table-primary" key={i}>
+              <th scope="row">{i + 1}</th>
+              <td>
+                <img
+                  src={user.picture.thumbnail}
+                  alt={user.name.first + " " + user.name.last}
+                />
+              </td>
+              <td>{user.location.state}</td>
+              <td>{user.name.first}</td>
+              <td>{user.name.last}</td>
+              <td>{user.phone}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Table;
