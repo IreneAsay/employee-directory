1effcb90b3e5ef31ddbdd1ea1712ab797bc6f466
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-26 10:49:45 +0800

    added handleSort function to sort the location data

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -5,50 +5,66 @@ import Table from "./Table";
 import { isDOMComponentElement } from "react-dom/test-utils";
 
 class Main extends Component {
-    state = {
-        users: [],
-        filterUsers: [],
-        inputValue: ""
-    };
-    componentDidMount() {
-        getUsers().then((res) => {
-            // console.log(res.data.results);
-            this.setState({ ...this.state, users: res.data.results });
-            this.setState({ ...this.state, filterUsers: res.data.results });
-        });
-    }
+  state = {
+    users: [],
+    filterUsers: [],
+    // sortLocation: [],
+    inputValue: "",
+  };
+  componentDidMount() {
+    getUsers().then((res) => {
+      // console.log(res.data.results);
+      this.setState({ ...this.state, users: res.data.results });
+      this.setState({ ...this.state, filterUsers: res.data.results });
+    });
+  }
+
+  handleSearchChange = (event) => {
+    // console.log(event.target.value)
+    this.setState({ ...this.state, inputValue: event.target.value });
+
+    const filter = event.target.value;
+    const filteredList = this.state.users.filter((item) => {
+      const values =
+        item.name.first.toLowerCase() + " " + item.name.last.toLowerCase();
+      console.log(filter, values);
+      if (values.indexOf(filter.toLowerCase()) !== -1) {
+        return item;
+      }
+    });
+    this.setState({ ...this.state, filterUsers: filteredList });
 
     // <onClick= {sortLocation}></onClick>
 
-    // const sortLocation = () => {
-    //     const compare = (a, b) = > {
-    //         return a.
+    // const sortedList = this.state.data.sort () => {
+
     //     }
+  };
 
-    handleSearchChange = (event) => {
-        // console.log(event.target.value)
-        this.setState({ ...this.state, inputValue: event.target.value });
-        const filter = event.target.value;
-        const filteredList = this.state.users.filter((item) => {
+  handleSort = () => {
+    // console.log(this.state.users);
+    const sortedList = this.state.users.sort((a, b) =>
+      a.location.state.localeCompare(b.location.state)
+    );
+    this.setState({ ...this.state, filterUsers: sortedList });
+  };
 
-            let values =
-                item.name.first.toLowerCase() + " " + item.name.last.toLowerCase();
-            console.log(filter, values);
-            if (values.indexOf(filter.toLowerCase()) !== -1) {
-                return item;
-            }
-        });
-        this.setState({ ...this.state, filterUsers: filteredList });
-    };
+  handleSort = () => {
+    // console.log(this.state.users);
+    const sortedList = this.state.users.sort((a, b) =>
+      a.location.state.localeCompare(b.location.state)
+    );
+    this.setState({ ...this.state, filterUsers: sortedList });
+  };
 
-    render() {
-        return (
-            <>
-                <NavBar handleSearchChange={this.handleSearchChange} />
-                <Table users={this.state.filterUsers} />
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <NavBar handleSearchChange={this.handleSearchChange} />
+        <Table users={this.state.filterUsers} handleSort={this.handleSort} />
+      </>
+    );
+  }
 }
 
 export default Main;
